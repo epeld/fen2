97ddcef9cfc6b4e1b90cd8953c18a4868be2db9f
Erik Peldan <erik.peldan@gmail.com>
2019-03-03 11:18:27 +0100

    Need to be able to parse ranks/files separately

--- square.lisp
+++ square.lisp
@@ -23,10 +23,28 @@
   "Parse out a chess square from a string like \"e4\" or similar"
   (unless (eql (length string) 2)
     (error "Not a valid square-string '~a'" string))
-  (let ((col (- (char-code (aref string 0)) (char-code #\a)))
-        (row (+ (char-code #\1) (- 7 (char-code (aref string 1))))))
-    (unless (<= 0 col 7)
+  (let ((col (read-file (aref string 0) nil))
+        (row (read-rank (aref string 1) nil)))
+    (unless col
       (error "Invalid file for '~a'" string))
-    (unless (<= 0 row 7)
+    (unless row
       (error "Invalid rank for '~a'" string))
     (make-square col row)))
+
+
+(defun read-rank (character &optional (error t))
+  "Returns the ROW-coordinate that a chess rank signifies"
+  (let ((row (+ (char-code #\1) (- 7 (char-code character)))))
+    (if (<= 0 row 7)
+        row
+        (when error
+          (error "Invalid rank for '~a'" character)))))
+
+
+(defun read-file (character &optional (error t))
+  "Returns the COLUMN-coordinate that a chess file signifies"
+  (let ((col (- (char-code character) (char-code #\a))))
+    (if (<= 0 col 7)
+        col
+        (when error
+          (error "Invalid file for '~a'" character)))))
